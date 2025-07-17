b4d163132f054ed1c6b0ba9c0f59bed767a44f54
satyayannam <71513870+satyayannam@users.noreply.github.com>
2025-07-17 14:49:28 -0400

    auto: 2025-07-17 14:49:27

---
+++ ascii.js
@@ -0,0 +1,18 @@
+const ascii = {
+  figlet: `
+   _____       _             
+  / ____|     | |            
+ | (___   __ _| |_ ___  _ __ 
+  \\___ \\ / _\` | __/ _ \\| '__|
+  ____) | (_| | || (_) | |   
+ |_____/ \\__,_|\\__\\___/|_|   
+  `,
+  neofetch: `
+  OS: SatyaOS 1.0
+  Shell: /bin/hack
+  Editor: Vim/VSCode hybrid
+  RAM: 64GB idea-fuel
+  Uptime: 88 hours
+  Location: /home/satya
+  `
+};
